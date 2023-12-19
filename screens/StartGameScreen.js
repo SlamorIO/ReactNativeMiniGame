87df270d23f4ced2cc92ab4dee4b35c4898fdc31
Ou Yang <ouyang1278@gmail.com>
2023-12-19 16:34:50 +0800

    changes

---
+++ screens/StartGameScreen.js
@@ -0,0 +1,12 @@
+import { TextInput, View } from "react-native";
+import PrimaryButton from "../components/PrimaryButton";
+
+export default function StartGameScreen() {
+  return (
+    <View>
+      <TextInput></TextInput>
+      <PrimaryButton>Reset</PrimaryButton>
+      <PrimaryButton>Confirm</PrimaryButton>
+    </View>
+  );
+}
